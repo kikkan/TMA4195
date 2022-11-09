2cb5a30bced0790fe9cd785f6cb103acf46c2c52
kikkan <kikkan_93@hotmail.com>
2022-11-09 13:49:17 +0100

    commented code and sparse matrix examples

--- ChrisJibberish/CN2DStack.py
+++ ChrisJibberish/CN2DStack.py
@@ -1,22 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.sparse import diags
+from scipy.sparse import diags, hstack
 
 # %% Functions
 
 
 # %% 2D
+# Units
 L = 1  # Length of cleft
 D = 0.2  # Diffusion coefficient
-k1 = 0.1  # Forward reaction coefficient
-km1 = 0.01  # Backward reaction coefficient
+k1 = 0.1  # Forward reaction coefficient (4e6)
+km1 = 0.01  # Backward reaction coefficient (5)
+rBar = 152  # receptors on membrane (1000/(1e-6)^2 * 2*pi*(220e-9))
+nBar = 5000  # Neurotransmitters per vesicle pop
+
+# Discretization
 n = 100  # m=n, x and y same discretization
 dx = L/n
 dt = dx**2
 alpha = D * dt/(dx)**2  # D * dt/(dx)**2
 
+# Sparse matrices
 K = (n+1)**2  # Need N*M nodes
 k = [alpha * np.ones(K-(n+1)), alpha * np.ones(K-1), (1-4*alpha) * np.ones(K),
      alpha * np.ones(K-1), alpha * np.ones(K-(n+1))]  # Constructing the coefficient matrix A
 offset = [-n-1, -1, 0, 1, n+1]
 A = diags(k, offset).toarray()
+
+offsetA = [-1, 0, 1]  # Offset for tridiag matrix
+Avals = []
+# TODO offset for block matrix lower and upper. simply diags on -n and n respectively.
+
+# TODO implement upper and lower boundary. What to do on sides of cleft?
--- ChrisJibberish/temp.py
+++ ChrisJibberish/temp.py
@@ -6,14 +6,29 @@ from scipy.sparse import diags, hstack
 # S = vstack([A, B]).toarray()
 # print(S)
 
-offset = np.array([-1, 0, 1])
+# offset = np.array([-1, 0, 1])
 
-d1 = diags([1, 2, 1], offset, (4, 4))
-d2 = diags([2, 1, 2], offset, (4, 4))
-d3 = np.hstack((d1.toarray(), d2.toarray()))
-# print(d3)
+# d1 = diags([1, 2, 1], offset, (4, 4))
+# d2 = diags([2, 1, 2], offset, (4, 4))
+# d3 = np.hstack((d1.toarray(), d2.toarray()))
+# # print(d3)
 
-# %% sparse stack
-d3s = hstack([d1, d2])
-d3sArr = d3s.toarray()
-print(d3sArr)
+# # %% sparse stack
+# d3s = hstack([d1, d2])
+# d3sArr = d3s.toarray()
+# print(d3sArr)
+
+
+# # %% stacked and sparse stacked matrix mult
+# n1 = d3s.shape[1]
+# print(n1)
+# v = np.array([1 for _ in range(n1)])
+# m = d3s@v
+# print(m)
+
+x = np.arange(10)
+y = x**2
+# print(x)
+# print(y)
+plt.plot(x, y)
+plt.show()
